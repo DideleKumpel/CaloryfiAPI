df20706d4f90c451db8d4f8915461a3a1aa48f6f
Dawid Mackiewicz <dawid2004m@gmail.com>
2025-11-30 20:51:56 +0100

    Chagne name form Controller to Service. Add endpoint for geting meal component and update weight of component

--- CaloryfiAPI/Controllers/MealComponentController.cs
+++ CaloryfiAPI/Controllers/MealComponentController.cs
@@ -1,4 +1,5 @@
-﻿using CaloryfiAPI.Data;
+﻿using Caloryfi.Model.DTO;
+using CaloryfiAPI.Data;
 using CaloryfiAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,27 +9,38 @@ namespace CaloryfiAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class MealComponentService: Controller
+    public class MealComponentController: Controller
     {
         private readonly AppDatabaseContext _context;
 
-        public MealComponentService(AppDatabaseContext context)
+        public MealComponentController(AppDatabaseContext context)
         {
             _context = context;
         }
 
         [Authorize]
         [HttpPost("AddComponent")]
-        public async Task<IActionResult> AddComponent(MealComponent mealComponent) {
+        public async Task<IActionResult> AddComponent([FromBody]MealComponentDTO newMealComponent) {
             if (!TryGetUserId(out int userId))
                 return BadRequest(new { message = "Error occurred while reading userID" });
-            if(mealComponent == null)
+            if(newMealComponent == null)
             {
                 return BadRequest(new { message = "Error mealComponent is null" });
             }
             try
             {
-                await _context.MealComponents.AddAsync(mealComponent);
+                MealComponent mealComponentExist = await _context.MealComponents.FirstOrDefaultAsync(m => m.MealId == newMealComponent.MealId && m.IngredientId == newMealComponent.IngredientId);
+                if (mealComponentExist != null)
+                {
+                    return Conflict(new { message = "Meal component already exists" });
+                }
+                mealComponentExist = new MealComponent
+                {
+                    MealId = newMealComponent.MealId,
+                    IngredientId = newMealComponent.IngredientId,
+                    Weight = newMealComponent.Weight
+                };
+                await _context.MealComponents.AddAsync(mealComponentExist);
                 _context.SaveChanges();
             }
             catch
@@ -39,7 +51,7 @@ namespace CaloryfiAPI.Controllers
         }
 
         [Authorize]
-        [HttpDelete("DeleteComponent")]
+        [HttpDelete("DeleteComponent/{IngridientId}/{MealId}")]
         public async Task<IActionResult> DeleteComponent(int IngridientId, int MealId)
         {
             if (!TryGetUserId(out int userId))
@@ -79,6 +91,30 @@ namespace CaloryfiAPI.Controllers
             {
                 return StatusCode(500, "An error occurred while processing your request.");
             }
+        }
+
+        [Authorize]
+        [HttpPost("UpdateWeight")]
+        public async Task<IActionResult> UpdateWeight([FromBody] MealComponentDTO UpdatedMeal)
+        {
+            if (!TryGetUserId(out int userId))
+                return BadRequest(new { message = "Error occurred while reading userID" });
+            try
+            {
+                var Meal = await _context.MealComponents.FirstOrDefaultAsync(m => m.MealId == UpdatedMeal.MealId && m.IngredientId == UpdatedMeal.IngredientId);
+                if (Meal == null)
+                {
+                    return NotFound(new { message = "Meal component not found" });
+                }
+                Meal.Weight = UpdatedMeal.Weight;
+                _context.MealComponents.Update(Meal);
+                _context.SaveChanges();
+            }
+            catch
+            {
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+            return Ok("Component weight has been updated");
         }
 
         private bool TryGetUserId(out int userId)
---
+++ CaloryfiAPI/DTO/MealComponentDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caloryfi.Model.DTO
+{
+    public class MealComponentDTO
+    {
+        public int MealId { get; set; }
+        public int IngredientId { get; set; }
+
+        public double Weight { get; set; }
+
+    }
+}
